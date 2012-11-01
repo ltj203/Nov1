c12118fab13fd9eb6465983eb624148d998744cc
Lisa <ljenkin2@hotmail.com>
2012-11-01 11:37:31 -0400

    Initial Commit

---
+++ Nov1/Nov1AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  Nov1AppDelegate.h
+//  Nov1
+//
+//  Created by Lisa Jenkins on 11/1/12.
+//  Copyright (c) 2012 Lisa Jenkins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Nov1AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
